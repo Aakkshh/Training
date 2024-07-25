772ca1ef046a206ef80b85b93831324bf7670df6
Ayush9112 <axj9112@mavs.uta.edu>
2024-07-25 08:47:17 -0500

    python 2

---
+++ python2.py
@@ -0,0 +1,47 @@
+# class student:
+#     def __init__(self,name,age,grades):
+#         self.name=name
+#         self.age=age
+#         self.grades=grades
+#         print(f'{name},{age},{grades}')
+
+# r=student('ayush',23,[90,99,200])
+
+print("-------------------------------------------------------------")
+
+# class bank:
+#     def __init__(self,acc,bal):
+#         self.acc=acc
+#         self.bal=bal
+#         print(f'{acc},{bal}')
+# s=bank(123344455,10000)
+
+print("-------------------------------------------------------------")
+
+# class rectangle:
+#     def __init__(self,l,b):
+#         self.l=l
+#         self.b=b
+#     def area(l,b):
+#         a=l*b
+#         print(a)
+#     def peri(l,b):
+#         p=2*(l+b)
+#         print(p)
+    
+# rectangle.area(5,3)
+# rectangle.peri(5,3)
+
+print("-------------------------------------------------------------")
+
+# class car:
+#     def __init__(self,make,model,year):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+#         print(f"{self.make},{self.model},{self.year}")
+
+# car('toyota','camry',2024)
+
+print("-------------------------------------------------------------")
+
